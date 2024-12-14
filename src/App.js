d2dad7c697bb6004e286c8a0ca7c8e036e52599f
Hitesh Khatri <hiteshkumarkhatri7410@gmail.com>
2024-12-14 16:25:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter ,Routes,Route} from "react-router-dom";
+import './App.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Navbar from "./components/Navbar";
+import React from "react";
+import { ThemeProvider } from "./theme-context";
+
+function App() {
+  return (
+    <ThemeProvider>
+      <BrowserRouter basename="/Dark-Light-Mode">
+         <Navbar/>
+         {/* routes */}
+
+         <Routes>
+           <Route path="/"       element={<Home />}/>
+           <Route path="/about"  element={<About />}/>
+           <Route path="/blog"   element={<Blog />}/>
+         </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
